a6bd5f263ff5bad521c4dad6d0a273795c8c0945
SonalRote1 <32531277+SonalRote1@users.noreply.github.com>
2024-07-23 11:45:14 +0530

    Delete component1.js

--- component1.js
+++
@@ -1,9 +0,0 @@
-import { LightningElement, api } from 'lwc';
-
-
-export default class Component1 extends LightningElement {
-    @api var1;
-    changefn(event){
-        this.var1=event.target.value;
-    }
-}
